95374fab06aed0de10efdbd24a401e1d49cf000e
MuhammadBK1 <muhammadbk755@gmail.com>
2024-08-25 18:15:55 +0500

    Fix: Functionality enhanced

--- time_detctor.py
+++ time_detctor.py
@@ -1,9 +1,15 @@
+name = input("Enter your name:")
+print("\t\t\tWelcome",name.upper())
 import time
 currenttime= time.strftime ('%H:%M:%S')
 print(currenttime)
-if (currenttime>='05' and currenttime<'12'):
+if (currenttime>='04' and currenttime<'10'):
   print("Good Morning,SIR")
-elif (currenttime>='12' and currenttime<='17'):
+elif (currenttime>='10' and currenttime<='13'):
+  print("Good Noon,SIR")
+elif(currenttime>='13' and currenttime<='17:30'):
   print("Good Afternoon,SIR")
-else:
+elif (currenttime>='17:30' and currenttime<='19'):
   print("Good Evening,SIR")
+else:
+  print("Good Night,SIR")
